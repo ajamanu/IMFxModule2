f67fef75a72436fa420fc73e76f8f97b66a01cbd
ajamanu <amanu@champequity.com>
2017-02-09 15:37:38 +1100

    Update

--- Module_2.R
+++ Module_2.R
@@ -10,5 +10,9 @@ fiji <- read.csv("./Fiji Dataset/Module_2_Fiji_fiji.csv")
 
 # Following along with video
 
-# Create lm for rgdp_fiji with rgdp_aus
-fit_1 <- lm(log(R))
+# chagne column names to lowercase
+names(fiji) <- tolower(names(fiji))
+
+# Create lm for rgdp_fiji with rgdp_aus (Note: this the long-run equation)
+fit_1 <- lm(log(rgdp_fiji) ~ log(rgdp_aus), data = fiji)
+summary(fit_1)
